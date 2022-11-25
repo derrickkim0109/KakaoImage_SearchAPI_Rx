9b58a5c0ea4fad1e584d7a547d052f345c4656c2
derrickkim0109 <derrickkim0109@naver.com>
2022-11-25 14:18:42 +0900

    feat: code-base 초기 설정

---
+++ KakaoImageSearching/KakaoImageSearching/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  KakaoImageSearching
+//
+//  Created by Derrick kim on 2022/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
